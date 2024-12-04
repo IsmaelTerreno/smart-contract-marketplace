58e7502c84b06e6eb6c78c12b902da2fd92a8e04
IsmaelTerreno <elismakof2002@gmail.com>
2024-12-04 19:23:08 -0300

    Refactor Marketplace test imports for clarity
    Remove unused imports from the Marketplace test file to enhance clarity and reduce clutter. This streamlining of imports helps maintain cleaner and more readable code, focusing only on the necessary dependencies for the tests.

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -1,11 +1,6 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const {assert} = require("ethers");
 
 describe("Marketplace", function () {
   let Marketplace;
